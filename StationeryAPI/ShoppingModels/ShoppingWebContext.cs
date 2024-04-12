96125285e226762b7cb59ef5acfda974d031bade
congkhanh091 <dbuicongkhanh@gmail.com>
2024-04-12 19:05:17 +0700

    123 merge

--- StationeryAPI/ShoppingModels/ShoppingWebContext.cs
+++ StationeryAPI/ShoppingModels/ShoppingWebContext.cs
@@ -39,7 +39,7 @@ public partial class ShoppingWebContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAP14404\\SQLEXPRESS;Initial Catalog=ShoppingWeb;Persist Security Info=True;User ID=sa;Password=123;Encrypt=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-NN7VM6HS\\SQLEXPRESS;Initial Catalog=ShoppingWeb;Persist Security Info=True;User ID=sa;Password=123456;Encrypt=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
